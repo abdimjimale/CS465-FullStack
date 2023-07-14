f69cda531859e173d17db47308a29780e19271d9
abdimjimale <abdisalam.jimale@snhu.edu>
2023-07-14 22:47:29 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.use('/', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'index.html')
+    res.sendFile(filePath)
+})
+
+module.exports = app
